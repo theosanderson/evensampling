4ae5af95197a703a5b7ba60dcb7d5b0ae2150dab
Theo Sanderson <theo@theo.io>
2021-02-22 20:31:55 +0000

    updates

---
+++ tests/test_sampler.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+import pytest
+
+from evensampling import sampler
+
+__author__ = "Your Name"
+__copyright__ = "Your Name"
+__license__ = "mit"
+
+
+def test_fib():
+    pass
